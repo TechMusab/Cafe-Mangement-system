51bd5f80517fcab3263658433b5556d2e2d9618b
Musab <joiya4636@gmail.com>
2025-04-18 00:42:37 -0700

    Update index.js

--- inventory-services/index.js
+++ inventory-services/index.js
@@ -21,30 +21,9 @@ const connectMongo = async () => {
     }
   }
 };
-/*
-/*i have cafe managemnt system in which these services includes
-customer-services inventroy services menu services
-order services payment services
-i have tasked with evluting a node.js microservices based cafe management sytem which suportd a customer emma who use cafe. she navigwates to appp to browse the menu check her loyalty points and other services.
-Cafe management system consist of 6 microservices
-1.Api gateway running on http://localhost:3000 serves as central entry point efficantly routng the app to apporprate microsrvices
-2.Menu service manges the cafe menu storing items in mongodb with details like itemid, name price and stock it respponds to get requests http://localhost:3000/menu 
-3.Order service is core handling Emma POST request http://localhost:3000/orders with selections(latte:1,muffin:1). It vallidates items by querying the menu service check emma customer id with customer serivce calculates the total and stores the order in mongodb it then updates stock by sending the post request to http://localhost:3000/inventory/update and awards loyalyy points
-via http://localhost:3000/customers/update-points 
-4.The inventory service maintains stock level in mongodb resppondign to order service update requst by reducing quanatites and verifying sufficient stock 
-5.The Custmer sservice manages emma profile storing id name email loyality points in mongodb. ti handles get requests to display cureent points and post reuqest to add points incentivizing her retuen visits with rewars like free coffee at 20 points 
-  6.payment service process emma payment thoriugh post requst http://localhost:3000/payments verifying the total matches the ordr by querying the roder service recoding the transcion to mongodb
-  github repo: https://github.com/zoya532/Lab_Sessional.git
-  implementation steps:
-to run this follow structed workflow inolcing steup canternizaton and continous integratioin:
-1.Install all the necessary dependencies and libraries for each microservice. This involves navigating to each directory and ensuring all the requeird nodejs packages are correctly installed 
-2. Conternize each microservice using a Dockerfile each dockerfile should define envoirnment setup inclding base image working dir dependency installtion source code copying and service startup 
-3.implement the CI/CD automation using github actions this includes creating workflow fils that automaticaly build dokcer images for each servce whenever changes are pushed to main branch. it should handle steps such as checking out the code setting up the build envoinment logging in docker hub  builfing the images and pushing them to registry before implementing you have to generate tour own github repo  and push the code
-finally sytem should start and run either locally or contairnized envoirnemnet
- */
+
 connectMongo().catch(console.error);
 
-// Menu Item Schema
 const menuItemSchema = new mongoose.Schema({
   id: { type: Number, required: true, unique: true },
   name: String,
